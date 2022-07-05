decd7c056a740d7f866ccfb6a554255c23eca09c
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-05 07:29:59 +0100

    use formatter

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@
 def uppercase(str):
     for i in str:
         islower = ord(i) >= 97 and ord(i) <= 122
-        print(chr(ord(i) - 32) if islower else i, end='')
+        print("{}".format(chr(ord(i) - 32) if islower else i), end='')
     print('\n')

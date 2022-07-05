0c035108cd44c660bd19e1f0954b984fffc3e386
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-05 21:18:02 +0100

    convert to script

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
-# <a value> + <b value> = <add(a, b) value> followed with a new line
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))

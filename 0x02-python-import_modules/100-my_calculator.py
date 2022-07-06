378b6b0d40a96c5184d0a5c213f5540c463a186a
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-06 18:07:34 +0100

    use module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,6 +2,7 @@
 
 if __name__ == "__main__":
     import sys
+    import calculator_1 as calc
 
     av = sys.argv
     ac = len(av) - 1
@@ -14,11 +15,14 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
+    a = int(av[1])
+    b = int(av[3])
+
     if av[2] == "+":
-        print("{} + {} = {}".format(av[1], av[3], int(av[1]) + int(av[3])))
+        print("{} + {} = {}".format(av[1], av[3], calc.add(a, b)))
     if av[2] == "-":
-        print("{} - {} = {}".format(av[1], av[3], int(av[1]) - int(av[3])))
+        print("{} - {} = {}".format(av[1], av[3], calc.sub(a, b)))
     if av[2] == "*":
-        print("{} * {} = {}".format(av[1], av[3], int(av[1]) * int(av[3])))
-    if av[2] == "/":
-        print("{} / {} = {}".format(av[1], av[3], int(av[1]) / int(av[3])))
+        print("{} * {} = {}".format(av[1], av[3], calc.mul(a, b)))
+    if av[2] == "/" or av[2] == "//":
+        print("{} / {} = {}".format(av[1], av[3], calc.div(a, b)))

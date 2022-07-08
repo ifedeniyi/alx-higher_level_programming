767aa86143cb05bfbec57b5a2a019f73729c2ea8
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-08 10:16:54 +0100

    pack tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c = (tuple_a[0] if len(tuple_a) else 0) + \
-        (tuple_b[0] if len(tuple_b) else 0)
-    tuple_d = (tuple_a[1] if len(tuple_a) > 1 else 0) + \
-        (tuple_b[0] if len(tuple_b) > 1 else 0)
+    tuple_a = tuple_a[:2] + ((0,) * (2 - len(tuple_a)))
+    tuple_b = tuple_b[:2] + ((0,) * (2 - len(tuple_b)))
 
-    return (tuple_c, tuple_d)
+    return tuple([x + y for x, y in zip(tuple_a, tuple_b)])

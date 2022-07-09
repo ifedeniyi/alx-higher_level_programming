2c45eee99bd842eae435e891f0179185726ee135
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-09 16:28:40 +0100

    fix

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,4 +3,6 @@
 def only_diff_elements(set_1, set_2):
     unique = list(i for i in set_1) + list(j for j in set_2)
 
+    unique = filter(lambda x: not(x in set_1 and x in set_2), unique)
+
     return set(unique)

dc6a8d001b5a3c749789ddeddb71f5034f233aff
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-09 20:46:59 +0100

    return a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    max_key = None
+    max_val = 0
+
+    if a_dictionary == None:
+        return None
+
+    for i in a_dictionary.keys():
+        if a_dictionary.get(i) > max_val:
+            max_key = i
+            max_val = a_dictionary.get(i)
+
+    return max_key

34b6c6f50623fbd5d4798400c5ac1cf86dc03c74
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-10 08:29:05 +0100

    add halting condition

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
+    if len[my_list] is 0:
+        return 0
+
     lhs = sum([x*y for x, y in my_list])
     rhs = sum([y for _, y in my_list])
 

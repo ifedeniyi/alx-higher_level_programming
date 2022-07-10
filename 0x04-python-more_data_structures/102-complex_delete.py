dcd3fd3d65d201b4de3223913755d9e273e66053
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-10 10:55:25 +0100

    fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,12 +2,17 @@
 
 def complex_delete(a_dictionary, value):
     final_dict = {}
+    updated = False
 
     for k, v in a_dictionary.items():
         if v == value:
+            updated = True
             continue
         final_dict[k] = v
-    
-    a_dictionary.clear()
-    a_dictionary.update(final_dict)
-    return final_dict
+
+    if updated:
+        a_dictionary.clear()
+        a_dictionary.update(final_dict)
+        return final_dict
+
+    return a_dictionary

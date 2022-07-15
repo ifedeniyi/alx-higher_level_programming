1dec86461030019d1e9af4edc23d244808f80f54
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-15 21:29:49 +0100

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Exports Square class"""
+
+
+class Square:
+    """Square class doing nothing useful yet.
+
+    Args:
+        size (int): Size of a square instance.
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not str:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

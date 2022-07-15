02b1ddffe0581c44b5a67d8adcee4c8b7333b0fc
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-15 21:10:05 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module exporting the Square class."""
+
+
+class Square:
+    """Square class doing nothing useful.
+
+    Args:
+        size (int): Size of the square.
+    """
+
+    def __init__(self, size):
+        self.__size = size

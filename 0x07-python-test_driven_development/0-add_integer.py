a57cf5c29727a0418f0bd9679a541d38dd96e7ad
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-17 02:20:03 +0100

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""Defines a function `add_integer` that adds 2 integers.
+"""This is the `0-add_integer` module.
+
+The `0-add_integer` module supplies one function, `add_integer()`.
 
 Example:
     >>> add_integer = __import__('0-add_integer').add_integer
+    >>> print(add_integer(1, 2))
 """
 
 
 def add_integer(a, b=98):
-    """`add_integer` function definition.
-    Adds two integers `a` and `b`.
+    """Adds two integers `a` and `b`.
+
+    Args:
+        a (int): First operand in addition operarion.
+        b (:obj:`int`, optional): Second operand in addition operation.
+
+    Raises:
+        TypeError: If either `a` or `b` are not valid integers.
+
+    Returns:
+        int: Sum of `a` and `b`.
     """
 
     allowed_types = [int, float]

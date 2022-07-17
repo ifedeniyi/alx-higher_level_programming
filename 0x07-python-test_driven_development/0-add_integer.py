ad78464f49df1ada30bbd7c251730b86606f0b1f
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-17 01:28:14 +0100

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Not sure what this does yet... stay tuned."""
+
+
+def add_integer(a, b=98):
+    """Returns the addition of two integers `a` and `b`.
+
+    Args:
+        a (int): First operand in addition operarion.
+        b (:obj:`int`, optional): Second operand in addition operation.
+
+    Returns:
+        (int) Result of adding `a` and `b`.
+    """
+    allowed_types = [int, float]
+
+    if type(a) not in allowed_types or type(b) not in allowed_types:
+        raise TypeError("a must be an integer or b must be an integer")
+
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")

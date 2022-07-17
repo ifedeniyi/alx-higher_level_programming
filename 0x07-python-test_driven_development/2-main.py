c95872a8e85c9972b8f8e73656992c5ebbecc510
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-17 12:43:02 +0100

    Divide a matrix

---
+++ 0x07-python-test_driven_development/2-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [2, 3, 6],
+    [8, 10, 12]
+]
+print(matrix_divided(matrix, 2))
+print(matrix)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is the `2-matrix_divided` module.
+
+The `2-matrix_divided` module supplies one function, `matrix_divided()`
+
+Example:
+    >>> matrix_divided = __import__("2-matrix_divided").matrix_divided
+    >>> new_matrix = matrix_divided([[2, 4], [6, 8]], 2)
+    >>> print(new_matrix)
+"""
+
+
+def matrix_divided(matrix, div):
+    """Divides all emalents of a matrix `matrix` by int `div`.
+
+    Args:
+        matrix (:obj:`list` of :obj:`list`): Matrix to transform.
+        div (int): Int to divide `matrix` elements by.
+
+    Returns:
+        (:obj:`list` of :obj:`list`): Transformed matrix.
+    """
+
+    return list(map(lambda x: list(map(lambda y: y / div, x)), matrix))

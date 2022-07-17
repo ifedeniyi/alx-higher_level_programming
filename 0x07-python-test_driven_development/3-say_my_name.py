d39cc1c616d94dd5bd4c352b9db542f83b9d227b
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-17 21:47:27 +0100

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This is the `3-say_my_name` module.
+
+The `say_my_name` module supplies one function, `say_my_name()`.
+
+Example:
+    >>> say_my_name = __import__("3-say_my_name").say_my_name
+    >>> say_my_name("Frank", "Sinatra")
+    My name is Frank Sinatra
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints string containing `first_name` and `last_name`.
+
+    Args:
+        first_name (string)
+        last_name (:obj:`string`, optional)
+
+    Raises:
+        TypeError: If either `first_name` or `last_name` are not strings
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))

ba8f02e9ace37537b6d3ebd4c2a8c7810eb51424
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-17 15:32:18 +0100

    more passing cases

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -5,5 +5,5 @@ matrix = [
     [1, 2, 3],
     [4, 5, 6]
 ]
-print(matrix_divided(matrix, 3))
+print(matrix_divided([[5.8, 4.5678], [3.88888, 10]], 2.5))
 print(matrix)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,6 +2,12 @@
 """This is the `2-matrix_divided` module.
 
 The `2-matrix_divided` module supplies one function, `matrix_divided()`
+
+Example:
+    >>> matrix_divided = __import__("2-matrix_divided").matrix_divided
+    >>> new_matrix = matrix_divided([[2, 4], [6, 8]], 2)
+    >>> print(new_matrix)
+    [[1, 2], [3, 4]]
 """
 
 

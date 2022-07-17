378037ecff41a8cb6c2ef0fe2ee73f2eb0a1040a
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-17 01:39:42 +0100

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,24 +3,19 @@
 
 
 def add_integer(a, b=98):
-    """Returns the addition of two integers `a` and `b`.
+    """Adds two integers `a` and `b`.
 
     Args:
         a (int): First operand in addition operarion.
         b (:obj:`int`, optional): Second operand in addition operation.
-
-    Returns:
-        (int) Result of adding `a` and `b`.
     """
 
     allowed_types = [int, float]
 
     if type(a) not in allowed_types:
         raise TypeError("a must be an integer")
-
     if type(b) not in allowed_types:
         raise TypeError("b must be an integer")
-
     return int(a) + int(b)
 
 

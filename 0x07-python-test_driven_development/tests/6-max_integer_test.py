0471956215273c3d4974a45f455117826e3e70cd
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-19 08:30:56 +0100

    test failing cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,7 +6,15 @@ max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test_max(self):
+    def test_passing(self):
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([2, 4, 20]), 20)
         self.assertEqual(max_integer([1000, 10000, 500000]), 500000)
+
+    def test_failing(self):
+        self.assertRaises(TypeError, max_integer, 1)
+        self.assertRaises(TypeError, max_integer, 1.3)
+        self.assertRaises(TypeError, max_integer, -3)
+        self.assertRaises(TypeError, max_integer, 2j)
+        self.assertRaises(TypeError, max_integer, [2j, 3])
+        self.assertRaises(TypeError, max_integer, [-2, "90"])

2d16b08050bee004b0d5756d37803a0e4fef45f2
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-22 21:51:21 +0100

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is the `4-inherits_from` module.
+
+The `4-inherits_from` module supplies one function, `inherits_from()`.
+"""
+
+
+def inherits_from(obj, a_class):
+    """check if `obj` is instance of `a_class`.
+
+    Args:
+        obj: object to check instance of
+        a_class: class to perform check against
+
+    Returns:
+        bool: True if `obj` is exact instance of `a_class`, False otherwise
+    """
+
+    return isinstance(obj, a_class)

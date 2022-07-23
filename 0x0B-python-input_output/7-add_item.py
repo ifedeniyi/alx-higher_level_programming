dec7240dad2c414627ff488f19db3721d4249511
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-23 15:59:45 +0100

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""""""
+"""Appends commandline args to json file"""
 import sys
+import os
+from pathlib import Path
+
 stojson = __import__("5-save_to_json_file").save_to_json_file
 lfromjson = __import__("6-load_from_json_file").load_from_json_file
-
 args = sys.argv[1:]
-# args = lfromjson(1)
-stojson(args, "add_item.json")
-sys.stdin
+
+final_obj = []
+
+if Path.is_file(Path.cwd() / 'add_item.json'):
+    final_obj = lfromjson("add_item.json")
+
+stojson([*final_obj, *args], "add_item.json")

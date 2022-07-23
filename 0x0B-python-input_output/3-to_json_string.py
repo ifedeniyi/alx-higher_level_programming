d874126a01e928923be9cde1b234f3c4a4612efe
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-23 10:57:37 +0100

    serializes a python object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The `3-to_json_string` module.
+
+It defines one function, `to_json_string()`.
+"""
+
+
+def to_json_string(my_obj):
+    """Serializes an object (string) to JSON
+
+    Args:
+        my_obj
+
+    Returns:
+        string: serialized JSON string
+    """
+
+    return json.dumps(my_obj)

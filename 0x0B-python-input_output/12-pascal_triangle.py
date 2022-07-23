99f64f2de342545c490fd8b98b46249bb0a77508
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-23 19:28:04 +0100

    refactor pascals triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,32 +9,19 @@ def pascal_triangle(n):
     """Returns a list of lists of integers representing
         the Pascal’s triangle"""
 
-    col = []
-
     if n <= 0:
         return []
 
-    for i in range(n):
-        row = []
+    col = [[1]]
 
-        if i == 0:
-            col.append([1])
-            continue
-
+    while len(col) != n:
         prev = col[-1]
-        # prev -> [1, 2, 1] -> len = 3
-        for j in range(len(prev) + 1):
-            if j == 0:
-                row.append(1)
-                continue
+        row = [1]
 
-            if j == len(prev):
-                row.append(1)
-                break
+        for i in range(len(prev) - 1):
+            row.append(prev[i] + prev[i+1])
 
-            result = prev[j] + prev[j - 1]
-            row.append(result)
-
+        row.append(1)
         col.append(row)
 
     return col

f4c963ca41456b4639339095a8ca5f70dc53bba0
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-28 23:19:52 +0100

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """The `base` module.
 
 It defines one class, `Base`.
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Unittest for `base` module functionality
 """
 import unittest
